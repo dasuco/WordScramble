261dff38bf9a60e8c1d10901a17f73f79402653d
DavidSucias <david.sucias@gft.com>
2021-07-14 12:36:09 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Sucias Colomer, David on 14/7/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
